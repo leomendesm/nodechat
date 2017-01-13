9daee6ff8b3bf47de68a20fc32c10659afed8fb5
leonardo miraglia <leonardo miraglia>
2017-01-13 00:07:53 -0200

    ip

--- public/js/init.js
+++ public/js/init.js
@@ -1,4 +1,4 @@
-let socket = io('http://localhost:3001');
+let socket = io('http://192.241.189.200:3001/');
 let room = localStorage.getItem("room");
 		socket.on(room, function (data) {
 			var chat = document.getElementById('chat');
